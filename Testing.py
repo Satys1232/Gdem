1faabeef4f1ba08a614a910bf98bb0e1508cf9b6
Satys1232 <23057045@kiit.ac.in>
2026-08-10 19:32:05 +0530

    practice question

---
+++ Testing.py
@@ -0,0 +1,15 @@
+# def park_fee_calc(n: int)->int:
+#     parking_charge = 0
+#     if n <= 2:
+#         parking_charge = n * 30
+#     elif 3 <= n <= 5:
+#         parking_charge = n * 25
+#     elif n > 5:
+#         parking_charge = n * 20
+
+#     if parking_charge > 150:
+#         parking_charge += 20
+#     return parking_charge
+
+# print(park_fee_calc(15))
+
